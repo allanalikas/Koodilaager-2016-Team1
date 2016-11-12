60a5bc248a959109545c15f0ff2338da67085c61
Allan <frozen_ranger5@hotmail.com>
2016-11-12 19:19:07 +0200

    merge error fix

--- Stimulator/enemy.py
+++ Stimulator/enemy.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pygame
 from character import Character
 from constants import *
@@ -14,5 +13,3 @@ class Enemy(Character):
 
     def draw(self, screen, cam_pos):
         screen.blit(self.icon, [self.x - cam_pos[0], self.y - cam_pos[1]])
-=======
->>>>>>> 80011f574ff578e743c901937a520f117d6620c4
