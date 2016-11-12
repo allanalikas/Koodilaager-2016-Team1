b63bae1eae467b2b59f35fcbc3ab3b9901fce02a
Caspar Sünter <caspar@synter.ee>
2016-11-12 13:08:39 +0200

    Inverted Wall and Ground colours and got rid of excess code

--- Stimulator/map.py
+++ Stimulator/map.py
@@ -3,19 +3,13 @@ from constants import *
 
 MAPX = screen_w // TILESIZE
 MAPY = screen_h // TILESIZE
+MAP_W = 22
 
-BLACK = (0,0,0)
-WHITE = (255, 255, 255)
-
+"""
 WALL = "#"
 GROUND = " "
-
-colours ={
-        WALL : BLACK,
-        GROUND : WHITE
-         }
-
-MAP_W = 22
+COUCH = "C"
+"""
 
 map1 =   """
 ######################
@@ -32,26 +26,6 @@ map1 =   """
 
 map1 = map1.replace('\n', '')
 
-
-'''
-def init(map_nr):
-
-    if map_nr == 1:
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-
-            for row in range(MAPX):
-                print("jouab")
-                for column in range(MAPY):
-                    print("jouab2")
-                    pygame.draw.rect(DISPLAYSURF, colours[map1[row][column]], (column*TILESIZE, row*TILESIZE, TILESIZE, TILESIZE))
-        
-            pygame.display.update()
-init(1)
-'''
 def draw(s, cam_pos):
     x = 5
     y = 3
@@ -76,7 +50,10 @@ def draw(s, cam_pos):
                                     TILESIZE)
 
             if value == ' ':
-                pygame.draw.rect(s, [0, 0, 0], tile_rect)
+                pygame.draw.rect(s, [255, 255, 255], tile_rect)
 
             elif value == '#':
-                pygame.draw.rect(s, [255, 255, 255], tile_rect)
+                pygame.draw.rect(s, [0, 0, 0], tile_rect)
+
+            elif value == 'C':
+                pygame.draw.rect(s, [255, 255, 0], tile_rect)
