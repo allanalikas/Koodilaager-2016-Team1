b6b50645b4c077292e00de47ba116d874f821fb6
Allan <frozen_ranger5@hotmail.com>
2016-11-12 19:16:27 +0200

    Basic enemy

--- Stimulator/enemy.py
+++ Stimulator/enemy.py
@@ -1,5 +1,15 @@
-<<<<<<< HEAD
 import pygame
-=======
-import pygame
->>>>>>> 362ffd4540caec3725e603827e667433b532217a
+from character import Character
+from constants import *
+
+
+class Enemy(Character):
+    def __init__(self, x, y, player_icon):
+        self.x = x
+        self.y = y
+        self.x_speed = 0
+        self.y_speed = 0
+        self.icon = player_icon
+
+    def draw(self, screen, cam_pos):
+        screen.blit(self.icon, [self.x - cam_pos[0], self.y - cam_pos[1]])
