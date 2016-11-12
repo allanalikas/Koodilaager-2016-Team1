7f59640f68540860874f5a48e10021b4a35fb6e7
Gert Jõgiste <gertjogiste@gmail.com>
2016-11-12 11:40:41 +0200

    Character and player

--- Stimulator/player.py
+++ Stimulator/player.py
@@ -2,12 +2,16 @@ import pygame
 from constants import *
 from character import Character
 
-class Player(Character):
-    def __init__(self):
+character_icon = pygame.image.load("player.jpg")
 
+class Player(Character):
+    def __init__(self, x, y, character_icon):
+        self.x = x
+        self.y = y
+        
     def on_event(self, event):
 
     def update(self):
 
     def draw(self, screen):
-
+        Player = screen.blit(character_icon, )
