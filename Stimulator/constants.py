4e8d6d6a75ed38594b7726bfc74dac421053ed7a
Gert Jõgiste <gertjogiste@gmail.com>
2016-11-12 13:17:35 +0200

    tegelane ekraanil

--- Stimulator/constants.py
+++ Stimulator/constants.py
@@ -2,6 +2,8 @@ screen_w, screen_h = 800, 600
 fps = 30
 TILESIZE = 50
 
+# 0 - up, 1 - right, 2 - down, 3 - left
+player_dir = 0
 
 class State_switcher(Exception):
     def __init__(self, value):
--- Stimulator/player.py
+++ Stimulator/player.py
@@ -3,8 +3,6 @@ from constants import *
 from character import Character
 
 
-
-
 class Player(Character):
     def __init__(self, x, y, player_icon):
         self.x = x
@@ -12,18 +10,10 @@ class Player(Character):
         self.icon = player_icon
         
     def on_event(self, event):
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
-                pass
-            elif event.key == pygame.K_a:
-                pass
-            elif event.key == pygame.K_s:
-                pass
-            elif event.key == pygame.K_d:
-                pass
+        pass
 
     def update(self):
         pass
 
     def draw(self, screen):
-        screen.blit(self.icon, [screen_w/2, screen_h/2])
+        screen.blit(self.icon, [screen_w/2 -25, screen_h/2 -25])
