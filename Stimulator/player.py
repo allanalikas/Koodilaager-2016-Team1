02db7ffc364f467b4aeb05c2e67f13551ce08311
Gert Jõgiste <gertjogiste@gmail.com>
2016-11-12 13:43:13 +0200

    began the moving programming

--- Stimulator/player.py
+++ Stimulator/player.py
@@ -7,13 +7,28 @@ class Player(Character):
     def __init__(self, x, y, player_icon):
         self.x = x
         self.y = y
+        self.x_speed = 0
+        self.y_speed = 0
         self.icon = player_icon
         
     def on_event(self, event):
-        pass
+        for e in pygame.event.get():
+            if e == pygame.KEYDOWN:
+               if event.key == pygame.K_UP:
+                   self.y_speed = -5
+
+               elif event.key == pygame.K_DOWN:
+                   self.y_speed = 5
+
+               elif event.key == pygame.K_LEFT:
+                   self.x_speed = -5
+
+               elif event.key == pygame.K_RIGHT:
+                   self.x_speed = +5
 
     def update(self):
-        pass
+        self.x += self.x_speed
+        self.y += self.y_speed
 
     def draw(self, screen):
         screen.blit(self.icon, [screen_w/2 -25, screen_h/2 -25])
