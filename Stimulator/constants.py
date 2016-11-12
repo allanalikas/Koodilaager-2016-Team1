b1f0112de1b468297c956d43961a5ef23203ec08
Void <raunoita@gmail.com>
2016-11-12 12:57:43 +0200

    Kaka kah

--- Stimulator/constants.py
+++ Stimulator/constants.py
@@ -1,3 +1,10 @@
 screen_w, screen_h = 800, 600
 fps = 30
 TILESIZE = 50
+
+class State_switcher(Exception):
+    def __init__(self, value):
+        Exception.__init__(self)
+        self.value = value
+    def ___str___(self):
+        return repr(self.value)
