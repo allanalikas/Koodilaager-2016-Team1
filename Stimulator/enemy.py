173af8b022899cfb19dea436d75cd330a8dedeea
Allan <frozen_ranger5@hotmail.com>
2016-11-12 21:13:17 +0200

    AI changes

--- Stimulator/enemy.py
+++ Stimulator/enemy.py
@@ -21,17 +21,31 @@ class Enemy(Character):
         # left = 1, up = 2, right = 3, down = 4
         possible_dirs = []
 
-        if map_data[tile_y][tile_x-1] == 0:
-            possible_dirs.append(1)
+        # print(map_data)
 
-        if map_data[tile_y-1][tile_x] == 0:
-            possible_dirs.append(2)
+        try:
+            if map_data[tile_y][tile_x-1] == 0:
+                possible_dirs.append(1)
+        except:
+            pass
 
-        if map_data[tile_y][tile_x+1] == 0:
-            possible_dirs.append(3)
+        try:
+            if map_data[tile_y-1][tile_x] == 0:
+                possible_dirs.append(2)
+        except:
+            pass
 
-        if map_data[tile_y+1][tile_x] == 0:
-            possible_dirs.append(4)
+        try:
+            if map_data[tile_y][tile_x+1] == 0:
+                possible_dirs.append(3)
+        except:
+            pass
+
+        try:
+            if map_data[tile_y+1][tile_x] == 0:
+                possible_dirs.append(4)
+        except:
+            pass
 
         if self.x > player.x and 1 in possible_dirs:
             self.x -= self.x_speed
